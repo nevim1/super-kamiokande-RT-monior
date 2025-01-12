bd7a039faaabedc548d274792884649abad830ab
nevim <ondrej.lupisek@gmail.com>
2025-01-12 17:57:50 +0100

    added/removed some comments for cleaner code

--- tkinterMonitor.py
+++ tkinterMonitor.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-#------------------------------------------------------------------------------
-# Note: 
-#   It's important that this code *not* interact directly with tkinter 
-#   stuff in the main process since it doesn't support multi-threading.
 import threading
 import requests
 from PIL import Image, ImageTk
@@ -12,6 +8,7 @@ import tkinter as tk
 import logging
 import argparse
 
+#---------- make enabling and disabling debug logs ----------------------------
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-v', '--Verbose', action='store_true')
@@ -19,12 +16,15 @@ parser.add_argument('-v', '--Verbose', action='store_true')
 arguments = parser.parse_args()
 
 if arguments.Verbose:
-    logging.basicConfig(format="%(levelname)s:%(asctime)s.%(msecs)03d: %(message)s", level=logging.DEBUG, datefmt="%H:%M:%S", force=True)
+    logging.basicConfig(format="%(levelname)s:%(asctime)s.%(msecs)03d: %(message)s", 
+                        level=logging.DEBUG, datefmt="%H:%M:%S", force=True)
 else:
     logging.disable()
 
+#----------- source url -------------------------------------------------------
 url = 'https://www-sk.icrr.u-tokyo.ac.jp/realtimemonitor/skev.gif'
 
+#---------- function for refreshing images on canvas --------------------------
 def refresh_image(canvas, img, image_id):
     response = requests.get(url)
 
@@ -33,20 +33,23 @@ def refresh_image(canvas, img, image_id):
         img = ImageTk.PhotoImage(pil_img)
         canvas.itemconfigure(image_id, image=img)
     else:
-        logging.error(f"response.status_code ins't 200, response.status_code acctually is {response.status_code}")
+        logging.error(f"""response.status_code ins't 200, response.status_code 
+        acctually is {response.status_code}""")
     # repeat every five seconds
     canvas.after(5000, refresh_image, canvas, img, image_id)  
 
-
+#---------- make root window --------------------------------------------------
 root = tk.Tk()
 root.configure(background='black')
 root.resizable(False, False)
 root.title('Super-Kamiokande python realtime monitor')
 
+#---------- make canvas for images --------------------------------------------
 canvas = tk.Canvas(root, height=999, width=1057)
 img = None  # initially only need a canvas image place-holder
 image_id = canvas.create_image(1057/2, 999/2, image=img)
 canvas.pack()
 
+#---------- start image refreshing and rendering of the whole window ----------
 refresh_image(canvas, img, image_id)
 root.mainloop()
